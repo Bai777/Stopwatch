d456bfa7f4ff1cd9bffc50fc1a5f9dd8c79898cc
Stepan <bai_777@mail.ru>
2022-02-02 20:11:17 +0300

    сдаю дз 4

---
+++ app/src/main/java/com/example/stopwatch/repository/StopwatchStateHolder.kt
@@ -0,0 +1,32 @@
+package com.example.stopwatch.repository
+
+import com.example.stopwatch.data.StopwatchState
+import com.example.stopwatch.datasource.StopwatchStateCalculator
+
+class StopwatchStateHolder(
+    private val stopwatchStateCalculator: StopwatchStateCalculator,
+    private val elapsedTimeCalculator: ElapsedTimeCalculator,
+    private val timestampMillisecondsFormatter: TimestampMillisecondsFormatter,
+) {
+    private var currentState: StopwatchState = StopwatchState.Paused(0)
+
+    fun start() {
+        currentState = stopwatchStateCalculator.calculateRunningState(currentState)
+    }
+
+    fun pause() {
+        currentState = stopwatchStateCalculator.calculatePausedState(currentState)
+    }
+
+    fun stop() {
+        currentState = StopwatchState.Paused(0)
+    }
+
+    fun getStringTimeRepresentation(): String {
+        val elapsedTime = when (val currentState = currentState) {
+            is StopwatchState.Paused -> currentState.elapsedTime
+            is StopwatchState.Running -> elapsedTimeCalculator.calculate(currentState)
+        }
+        return timestampMillisecondsFormatter.format(elapsedTime)
+    }
+}
